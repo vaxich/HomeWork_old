8a83394927f02c181fa271671dce0a51e4e55407
vaxich <vaxich@rambler.ru>
2022-04-25 13:06:25 +0300

    пишем что делели

--- my-app/src/p2-homeworks/h5/Routes.tsx
+++ my-app/src/p2-homeworks/h5/Routes.tsx
@@ -1,27 +1,38 @@
 import React from 'react'
+import { Switch } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
+import { Route} from 'react-router-dom'
+import Error404 from './pages/Error404'
+import Junior from './pages/Junior'
+import JuniorPlus from './pages/JuniorPlus'
+import PreJunior from './pages/PreJunior'
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
-    // add paths
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior-plus'
+    
 }
 
 function Routes() {
     return (
         <div>
             {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+            <Switch>
 
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-            {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
+            <Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
 
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
-                // add routes
+            <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
+            <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
+            <Route path={PATH.JUNIOR_PLUS} render={() => <JuniorPlus/>}/>
+               
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+            <Route render={() => <Error404/>}/>
 
-            {/*</Switch>*/}
+            </Switch>
         </div>
     )
 }
---
+++ my-app/src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function JuniorPlus() {
+    return (
+        <div>
+            JuniorPlus
+        </div>
+    )
+}
+
+export default JuniorPlus
