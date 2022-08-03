31be0149d9465f159f8952040fdc3a338a5b3458
Pavel Dedelyuk <vaxich@rambler.ru>
2022-08-03 14:04:09 +0300

    done 9 lessson

--- my-app/src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ my-app/src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HW3 from "../../h3/HW3";
+import HW9 from "../../h9/HW9";
 
 function JuniorPlus() {
     return (
         <div>
             JuniorPlus
             <HW3/>
+            <HW9 />
         </div>
     )
 }
